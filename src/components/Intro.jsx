f74ba82857df7cef6956241c99c6cbb3807d4864
flvffywvffy <flvffywvffy@gmail.com>
2022-11-19 08:43:27 +0000

    Added component files with basic html.

---
+++ src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Intro() {
+    return (
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Hey, I'm Marcus.</h1>
+            <p>Web & Game Developer</p>
+        </div>
+    )
+}
+
+export default Intro;
